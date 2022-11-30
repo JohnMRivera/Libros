9a878aaed2bef313a643a7101247677bae6e0441
JohnMRivera <johnrivera310802@gmail.com>
2022-11-30 00:39:30 -0600

    Se agregan rutas de clientes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ControladorVistas;
+use App\Http\Controllers\controladorClientesBD;
+use App\Http\Controllers\controladorLibrosBD;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +23,24 @@ use App\Http\Controllers\ControladorVistas;
 Route::controller(ControladorVistas::class)->group(function(){
     Route::get('/','vistaPrincipal')->name('pri');
     Route::get('registro_libros','vistaRegistroLibros')->name('reglib');
-});;
+});
 
 Route::post('guardarLibro',[ControladorVistas::class, 'procesarLibro'])->name('gualib');
+
+Route::controller(controladorClientesBD::class)->group(function(){
+
+    Route::get('cliente/create','create')->name('cliente.create');
+
+    Route::get('cliente/index','index')->name('cliente.index');
+
+    Route::post('cliente/store','store')->name('cliente.store');
+
+    Route::get('cliente/{id}/edit','edit')->name('cliente.edit');
+
+    Route::put('cliente/{id}/update','update')->name('cliente.update');
+
+    Route::get('cliente/{id}/show','show')->name('cliente.show');
+
+    Route::delete('cliente/{id}/destroy','destroy')->name('cliente.destroy');
+
+});
